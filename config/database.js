dd3782c738d132142fbf76d84e4db8b03be4c216
paolo <paolojoaoparis@gmail.com>
2026-02-03 14:52:42 +0100

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false, // passe à true si tu veux voir les requêtes SQL
+  },
+);
+
+export default sequelize;
